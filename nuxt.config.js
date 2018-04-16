c403792e1c5c013e56a1836af6602909a38a8715
r <admin@linkvehicle.com>
2018-04-16 17:25:48 -0400

    cms is in

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,13 @@
+var glob = require('glob');
+var path = require('path');
+
+// Enhance Nuxt's generate process by gathering all content files from Netifly CMS
+// automatically and match it to the path of your Nuxt routes.
+// The Nuxt routes are generate by Nuxt automatically based on the pages folder.
+var dynamicRoutes = getDynamicPaths({
+  '/blog': 'blog/posts/*.json'
+});
+
 module.exports = {
   /*
   ** Headers of the page
@@ -27,6 +37,10 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+
+  generate: {
+    routes: dynamicRoutes
+  },
   /*
   ** Build configuration
   */
@@ -45,4 +59,20 @@ module.exports = {
       }
     }
   }
+}
+
+
+/**
+ * Create an array of URLs from a list of files
+ * @param {*} urlFilepathTable
+ */
+function getDynamicPaths(urlFilepathTable) {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map(url => {
+      var filepathGlob = urlFilepathTable[url];
+      return glob
+        .sync(filepathGlob, { cwd: 'content' })
+        .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
+    })
+  );
 }
