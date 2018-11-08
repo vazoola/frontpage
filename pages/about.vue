77ef62522ba9855187bd9c1d776d0728fc10a8fd
jesseleejohnston <jesseleejohnston@gmail.com>
2018-11-07 20:23:29 -0800

    Getting familiar with the project and adding about section.

---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <nav-bar />
+
+        <section class="section technology-hero">
+        	<div class="container">
+            	<div class="columns">
+  					<div class="column">
+  						<div class="hero-text">
+	  						<h2>About</h2>
+	  						<hr class="blue" />
+	  					</div>
+  					</div>
+  				</div>
+			</div>
+        </section>
+
+
+        hello world
+        <footer-bar />
+    </div>
+</template>
+<script>
+import NavBar from '~/components/NavBar.vue'
+import FooterBar from '~/components/FooterBar.vue'
+
+
+export default {
+      components: {
+      NavBar,
+      FooterBar
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
